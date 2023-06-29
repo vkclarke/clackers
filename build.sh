3ae5642cc69c485eea3ccfc0a361bc1e48898d0b
Vincent Clarke <vincent@windows97.space>
2023-06-28 21:15:34 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- SONCRA.ASM, clackers.mdrv
+wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- SONCRA.ASM, clackers.mdrv, clackers.sym, clackers.lst | dos2unix
+dos2unix -f clackers.lst
+md5sum original.mdrv
+md5sum clackers.mdrv
