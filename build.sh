49a413b0e0b7446b72d2066289e960ecce4938cb
Vincent Clarke <vincent@windows97.space>
2023-06-28 22:48:53 -0400

    have build script generate s-record

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- SONCRA.ASM, clackers.mdrv, clackers.sym, clackers.lst | dos2unix
+wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /ps SONCRA.ASM, clackers.srec | dos2unix
+wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p SONCRA.ASM, clackers.mdrv, clackers.sym, clackers.lst | dos2unix
 dos2unix -f clackers.lst
 md5sum original.mdrv
 md5sum clackers.mdrv
