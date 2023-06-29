ae0e1469b7e5fdce874058ab95cabc5c72248424
Vincent Clarke <vincent@windows97.space>
2023-06-29 00:17:05 -0400

    remove absolute-word optimization

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wine ASM68K.EXE /o op+ /o ow+ /ps SONCRA.ASM, clackers.srec | dos2unix
-wine ASM68K.EXE /o op+ /o ow+ /p SONCRA.ASM, clackers.mdrv, clackers.sym, clackers.lst | dos2unix
+wine ASM68K.EXE /o op+ /ps SONCRA.ASM, clackers.srec | dos2unix
+wine ASM68K.EXE /o op+ /p SONCRA.ASM, clackers.mdrv, clackers.sym, clackers.lst | dos2unix
 dos2unix -f clackers.lst
 md5sum original.mdrv
 md5sum clackers.mdrv
