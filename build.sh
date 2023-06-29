cadc8238f3a24b3585097e1f2b19ef56be1b87b2
Vincent Clarke <vincent@windows97.space>
2023-06-28 20:42:58 -0400

    ok

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- SONCRA.ASM, SCBUILT.BIN
+wine ASM68K.EXE /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- SONCRA.ASM, clackers.mdrv
